e24ff9035114461b30073e8297617e6a4557e60e
Arnout Engelen <arnout@bzzt.net>
2021-10-06 15:37:41 +0200

    Move modules to their own file

--- build.nix
+++ build.nix
@@ -18,57 +18,7 @@ let
   #  rev = "v3.1.0";
   #  hash = "sha256-zWkgcRyihgeughynqcuvMdBMjiTh1DD694A0sCEM3oU=";
   #};
-  value-suggest =
-    fetchFromGitHub {
-      owner = "Xentropics";
-      repo = "ValueSuggest";
-      # 'ndeterms' branch
-      rev = "fb28c1afa43169adf0cd8532e636161c83835626";
-      hash = "sha256-bI9qz0+convjcvz8z/uFwD5/h+tTfwdVRBlDOfvZHf4=";
-    };
-  generic =
-    fetchFromGitLab {
-      owner = "Daniel-KM";
-      repo = "Omeka-S-module-Generic";
-      rev = "a59e2b5fc32b725a5837b4a11ba7baaba78534ba";
-      hash = "sha256-XhojKehNefAH5UhQgL1RRxQ1mBK1fdj0BRRIbquhQsI=";
-    };
-  #clean-url = /home/aengelen/dev/Omeka-S-module-CleanUrl;
-  clean-url = stdenv.mkDerivation {
-    pname = "Omeka-S-module-CleanUrl";
-    version = "snapshot";
-    src =
-      fetchFromGitLab {
-        owner = "Daniel-KM";
-        repo = "Omeka-S-module-CleanUrl";
-        rev = "b8a0f54300f2dda4d664cca0ffeea135031902a8";
-        hash = "sha256-+ahR2IX5WKi1t7r8ki3x+3h9doBbwUx73r6PGLzEFpU=";
-      };
-    patches = [
-      # avoid strict mysql query checking
-      (fetchpatch {
-        url = "https://gitlab.com/Daniel-KM/Omeka-S-module-CleanUrl/-/commit/e2b2bdad8ed5f233e4b3888391f3b17b54e7f265.patch";
-        sha256 = "sha256-r6wdz/ei84s/sOtr7bLW7cKxAuZBT4pKHzVFNvVwf2Y=";
-      })
-      # fix fetchAllAssociative output
-      (fetchpatch {
-        url = "https://gitlab.com/Daniel-KM/Omeka-S-module-CleanUrl/-/commit/51abdb1527f8fadfd74cea4b9df24b5c3652a144.patch";
-        sha256 = "199akm2104fgiph2m4ii0mahim0pjdjjk8i5p02pigyj9ngb0psv";
-      })
-    ];
-    installPhase = ''
-      runHook preInstall
-      cp -r . $out
-      runHook postInstall
-    '';
-  };
-  #clean-url =
-  #  fetchFromGitLab {
-  #    owner = "Daniel-KM";
-  #    repo = "Omeka-S-module-CleanUrl";
-  #    rev = "e841ea1804fe584b10da22fc5f03b1272b64b71e";
-  #    hash = "sha256-rCtxr/RrmOAWy9EXnmzVftDE0QPC1spm7GZad8ussH0=";
-  #  };
+  omeka-s-modules = callPackage ./omeka-s-modules.nix {};
   nginxPort = "8080";
   nginxConf = writeText "nginx.conf" ''
     user root nobody;
@@ -177,9 +127,9 @@ let
       chmod a+rwx webroot/logs/*
 
       chmod a+rwx webroot/modules
-      cp -r ${value-suggest} webroot/modules/ValueSuggest
-      cp -r ${generic} webroot/modules/Generic
-      cp -r ${clean-url} webroot/modules/CleanUrl
+      cp -r ${omeka-s-modules.value-suggest} webroot/modules/ValueSuggest
+      cp -r ${omeka-s-modules.generic} webroot/modules/Generic
+      cp -r ${omeka-s-modules.clean-url} webroot/modules/CleanUrl
       chmod a-w webroot/modules
     '';
     config = {
---
+++ omeka-s-modules.nix
@@ -0,0 +1,59 @@
+{ fetchFromGitHub
+, fetchFromGitLab
+, stdenv
+, fetchpatch
+}:
+
+{
+  value-suggest =
+    fetchFromGitHub {
+      owner = "Xentropics";
+      repo = "ValueSuggest";
+      # 'ndeterms' branch
+      rev = "fb28c1afa43169adf0cd8532e636161c83835626";
+      hash = "sha256-bI9qz0+convjcvz8z/uFwD5/h+tTfwdVRBlDOfvZHf4=";
+    };
+  generic =
+    fetchFromGitLab {
+      owner = "Daniel-KM";
+      repo = "Omeka-S-module-Generic";
+      rev = "a59e2b5fc32b725a5837b4a11ba7baaba78534ba";
+      hash = "sha256-XhojKehNefAH5UhQgL1RRxQ1mBK1fdj0BRRIbquhQsI=";
+    };
+  #clean-url = /home/aengelen/dev/Omeka-S-module-CleanUrl;
+  clean-url = stdenv.mkDerivation {
+    pname = "Omeka-S-module-CleanUrl";
+    version = "snapshot";
+    src =
+      fetchFromGitLab {
+        owner = "Daniel-KM";
+        repo = "Omeka-S-module-CleanUrl";
+        rev = "b8a0f54300f2dda4d664cca0ffeea135031902a8";
+        hash = "sha256-+ahR2IX5WKi1t7r8ki3x+3h9doBbwUx73r6PGLzEFpU=";
+      };
+    patches = [
+      # avoid strict mysql query checking
+      (fetchpatch {
+        url = "https://gitlab.com/Daniel-KM/Omeka-S-module-CleanUrl/-/commit/e2b2bdad8ed5f233e4b3888391f3b17b54e7f265.patch";
+        sha256 = "sha256-r6wdz/ei84s/sOtr7bLW7cKxAuZBT4pKHzVFNvVwf2Y=";
+      })
+      # fix fetchAllAssociative output
+      (fetchpatch {
+        url = "https://gitlab.com/Daniel-KM/Omeka-S-module-CleanUrl/-/commit/51abdb1527f8fadfd74cea4b9df24b5c3652a144.patch";
+        sha256 = "sha256-5UdRyiq5c/iQcUiCEb1rNt71KzhAxr2Wo7Gtl+hFDiI=";
+      })
+    ];
+    installPhase = ''
+      runHook preInstall
+      cp -r . $out
+      runHook postInstall
+    '';
+  };
+  #clean-url =
+  #  fetchFromGitLab {
+  #    owner = "Daniel-KM";
+  #    repo = "Omeka-S-module-CleanUrl";
+  #    rev = "e841ea1804fe584b10da22fc5f03b1272b64b71e";
+  #    hash = "sha256-rCtxr/RrmOAWy9EXnmzVftDE0QPC1spm7GZad8ussH0=";
+  #  };
+}
